858c415bec20b918248da6caf5ec99046b64f590
guille1999utp <guillermo.penaranda@utp.edu.co>
2021-10-18 01:08:24 -0500

    terminado sin responsive

--- src/componentes/Iniciar-sesion.js
+++ src/componentes/Iniciar-sesion.js
@@ -9,7 +9,7 @@ function Iniciarsesion() {
    <input className='inputl' type='email' placeholder='ingrese correo'/>
    <h3 className='ajus'>Contraseña</h3>
    <input className='inputl' type='password' placeholder='ingrese contraseña'/>
-   <button type='submit'> ingresar</button>
+   <button type='submit' className='buttoningresar'> ingresar</button>
    <h3 className='ajus'>no tienes cuenta?</h3>
    <a href='/register' className='am' >Registrarme</a>
     </form>
