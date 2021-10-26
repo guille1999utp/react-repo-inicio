8eb580006fc7c5578f202bae6b0ee0c4e78e4d88
guille1999utp <guillermo.penaranda@utp.edu.co>
2021-10-26 00:53:54 -0500

    parte chat

--- src/componentes/Chat.js
+++ src/componentes/Chat.js
@@ -6,12 +6,40 @@ function Chat() {
     <div className='fondochat'>
     <div className='fondonegrochat'></div>
     <div className='cajachat'>
-    <div className='usuariosactivos'>
+        <div className='usuariosactivos'>
+          <div className='chatperfil'>
+          <i class='bx bx-user' ></i>
+          <div>
+          <i class='bx bxs-chat chaticon'></i>
+          <i class='bx bx-dots-vertical-rounded menuchat'></i>
+          </div>
+          </div>
+          <div className='usuariochatperfil'>
 
-</div>
-<div className='mensajesusuarios'>
+          </div>
+          <div className='usuariochatperfil'>
 
-</div>
+          </div>
+          <div className='usuariochatperfil'>
+
+          </div>
+          <div className='usuariochatperfil'>
+
+          </div>
+
+
+        </div>
+        <div className='mensajesusuarios'>
+          <div className='paletacomandochat'>
+         
+          </div>
+          <div className='chatuses'>
+
+          </div>
+          <div className='paletachat'>
+
+          </div>
+        </div>
     </div>
     
    
