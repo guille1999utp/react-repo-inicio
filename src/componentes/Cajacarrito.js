19582b900bf7ef71256a4ebabacbcb9694487c98
guille1999utp <guillermo.penaranda@utp.edu.co>
2021-11-24 00:20:21 -0500

    f

---
+++ src/componentes/Cajacarrito.js
@@ -0,0 +1,43 @@
+import './Cajasolicitud.css';
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Cajasolicitud({producto,descripsion,urlfoto,id,carrito,setCarrito}) {
+    const productoa = producto.trim().slice(0,70);
+    const descripsiona = descripsion.trim().slice(0,150);
+  
+    const onDelete = () => {
+     const borrar = carrito.filter(element =>element.id !== id);
+     console.log(borrar);
+     setCarrito([...borrar]);
+       };
+    return (
+      <>
+      <div className="productocarrito">
+          <img
+            src={urlfoto}
+            className="img-producto"
+            alt="img"
+          ></img>
+          <div className="caracteristicaproductocarrito">
+            <div className="flexcolum">
+              <Link to="/producto/45" className="mostrartextotitulocarrito">
+                {productoa}
+              </Link>
+              <h3 className="cbasica">
+               {descripsiona}
+              </h3>
+              <span className="enviogratiscarrito">Envio gratis</span>
+              <button type="button" className="botoneliminarcarrito" onClick={onDelete}>
+                Eliminar
+              </button>
+            </div>
+          </div>
+        </div>
+        <hr></hr>
+              </>
+    );
+  }
+  
+  export default Cajasolicitud;
+  
--- src/componentes/Carrito.js
+++ src/componentes/Carrito.js
@@ -1,93 +1,64 @@
-import React from "react";
+import React ,{useState} from "react";
 import "./Carrito.css";
 import { Link } from "react-router-dom";
+import Cajacarrito from "./Cajacarrito";
+
 const Carrito = () => {
+  let infocarrito = [
+    {
+      id: 1,
+      producto:
+        "computador gamer",
+      descripsion: 'Jajaja qu,e onda señor Rivas, como está Yo estoy muy bien cansada como siempre por la serie jeje Yo estoy muy bien cansada como siempre por la serie jeje ',
+      urlfoto:'https://www.alkosto.com/medias/192876259719-001-750Wx750H?context=bWFzdGVyfGltYWdlc3wzNjU5MDJ8aW1hZ2UvcG5nfGltYWdlcy9oYzEvaGQ1LzkwNzg5NDYwMDUwMjIucG5nfGRjMzVlZjBkYzgwN2Y2ZWZhZGQxNmIxMjhhODg3NzU5NmMwNjNkN2I4OTc1NzMzY2NjNTA3YTQ3OTUxNjA2NGQ'
+    },
+    {
+      id: 2,
+      producto:"computador gamer",
+      descripsion: 'Jajaja qu,e onda señor Rivas, como está Yo estoy muy bien cansada como siempre por la serie jeje Yo estoy muy bien cansada como siempre por la serie jeje ',
+      urlfoto:'https://http2.mlstatic.com/D_NQ_NP_826178-MCO44786840309_022021-O.jpg'
+    },
+    {
+      id: 3,
+      producto:
+        "computador gamer",
+      descripsion: 'Jajaja qu,e onda señor Rivas, como está Yo estoy muy bien cansada como siempre por la serie jeje Yo estoy muy bien cansada como siempre por la serie jeje ',
+      urlfoto:'https://falabella.scene7.com/is/image/FalabellaCO/13166500_1?wid=800&hei=800&qlt=70'
+    },
+    {
+      id: 4,
+      producto:
+        "computador gamer",
+      descripsion: 'Jajaja qu,e onda señor Rivas, como está Yo estoy muy bien cansada como siempre por la serie jeje Yo estoy muy bien cansada como siempre por la serie jeje ',
+      urlfoto:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSTIUoORhUjAt4aoZIWn6VYAJebLwoDQPdXUw&usqp=CAU'
+    },
+    {
+      id: 5,
+      producto:
+        "computador gamer",
+      descripsion: 'Jajaja qu,e onda señor Rivas, como está Yo estoy muy bien cansada como siempre por la serie jeje Yo estoy muy bien cansada como siempre por la serie jeje ',
+      urlfoto:'https://tecnobits.net/wp-content/uploads/2018/08/RAZER-BLADE-1555.jpg'
+    },
+  ];
+  const [carrito, setCarrito] = useState(infocarrito);
+
+
   return (
     <div className="fondocarrito">
       <div className="conteinerproductoseleccionado">
-        <div className="flexrow">
+        <div className="flexro">
           <Link to="/carrito/" className="botoncarrito">
-            Carrito(0)
+            Carrito({carrito.length})
           </Link>
           <Link to="/carrito/guardado" className="botoncarrito">
-            Guardado(5)
+            Guardado(0)
           </Link>
         </div>
         <hr></hr>
-        <div className="productocarrito">
-          <img
-            src="https://fondosmil.com/fondo/17010.jpg"
-            className="img-producto"
-            alt="img"
-          ></img>
-          <div className="caracteristicaproductocarrito">
-            <div className="flexcolum">
-              <Link to="/producto/45" className="mostrartextotitulocarrito">
-                Monitor Samsung Gaming 27 Curvo Fhd Nvidia G-sync 240hz
-              </Link>
-              <h3 className="cbasica">
-                Color: Dark blue gray, Voltaje: 100V/240V
-              </h3>
-              <span className="enviogratiscarrito">Envio gratis</span>
-              <button type="button" className="botoneliminarcarrito">
-                Eliminar
-              </button>
-            </div>
-            <div className="flexcolum"></div>
-            <div className="flexcolum"></div>
-          </div>
+        {carrito.map((carro) =>(
+        <Cajacarrito producto={carro.producto} descripsion={carro.descripsion} urlfoto={carro.urlfoto} id={carro.id} carrito={carrito} setCarrito={setCarrito}></Cajacarrito>
+        ))}
         </div>
-        <hr></hr>
-
-        <div className="productocarrito">
-          <img
-            src="https://fondosmil.com/fondo/17010.jpg"
-            className="img-producto"
-            alt="img"
-          ></img>
-          <div className="caracteristicaproductocarrito">
-            <div className="flexcolum">
-              <Link to="/producto/45" className="mostrartextotitulocarrito">
-                Monitor Samsung Gaming 27 Curvo Fhd Nvidia G-sync 240hz
-              </Link>
-              <h3 className="cbasica">
-                Color: Dark blue gray, Voltaje: 100V/240V
-              </h3>
-              <span className="enviogratiscarrito">Envio gratis</span>
-              <button type="button" className="botoneliminarcarrito">
-                Eliminar
-              </button>
-            </div>
-            <div className="flexcolum"></div>
-            <div className="flexcolum"></div>
-          </div>
-        </div>
-        <hr></hr>
-        <div className="productocarrito">
-          <img
-            src="https://fondosmil.com/fondo/17010.jpg"
-            className="img-producto"
-            alt="img"
-          ></img>
-          <div className="caracteristicaproductocarrito">
-            <div className="flexcolum">
-              <Link to="/producto/45" className="mostrartextotitulocarrito">
-                Monitor Samsung Gaming 27 Curvo Fhd Nvidia G-sync 240hz
-              </Link>
-              <h3 className="cbasica">
-                Color: Dark blue gray, Voltaje: 100V/240V
-              </h3>
-              <span className="enviogratiscarrito">Envio gratis</span>
-              <button type="button" className="botoneliminarcarrito">
-                Eliminar
-              </button>
-            </div>
-            <div className="flexcolum"></div>
-            <div className="flexcolum"></div>
-          </div>
-        </div>
-        <hr></hr>
-      </div>
     </div>
   );
 };
