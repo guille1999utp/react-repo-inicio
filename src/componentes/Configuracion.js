5e262494ab3525a1e4f6a26510975583d36d8e23
guille1999utp <guillermo.penaranda@utp.edu.co>
2021-12-28 12:40:52 -0500

    agregar imagenes

--- src/componentes/Configuracion.js
+++ src/componentes/Configuracion.js
@@ -12,6 +12,8 @@ function Configuracion() {
   const miusuario =  useSelector(yo => yo.infoUsuario);
   const {socket} = useContext(SocketContext);
   const [guardarboton, setguardar] = useState(false);
+  const [guardarfoto, setguardarfoto] = useState(false);
+
 
   const [state, setState] = useState({
 Direccion:'',
@@ -53,7 +55,10 @@ Representantelegal:''
     secure_url:"https://res.cloudinary.com/dmgfep69f/image/upload/v1640536316/orgeial7kefv2dzsdqqt.webp",
     public_id: 0
   });
-
+  const [urlfotos, setUrlfotos] = useState({
+    secure_url:"https://res.cloudinary.com/dmgfep69f/image/upload/v1640536316/orgeial7kefv2dzsdqqt.webp",
+    public_id: 0
+  });
   const onSubmit = async(e) => {
     e.preventDefault();
   try{
@@ -100,16 +105,25 @@ Swal.fire({
     const file = e.target.files[0];
     setUrl(file);
   }
-
+  const onFilesavefoto  = async(e) =>{
+    const file = e.target.files[0];
+    setUrlfotos(file);
+  }
   const onFile  = () =>{
     document.querySelector('#fileperfil').click();
   }
-
+  const onFilefoto  = () =>{
+    document.querySelector('#filedes').click();
+  }
   const reset  = () =>{
   setUrl({
     secure_url:"https://res.cloudinary.com/dmgfep69f/image/upload/v1640536316/orgeial7kefv2dzsdqqt.webp",
     public_id: 0
   })
+  }
+  const cancelar = () =>{
+    setguardar(false);
+    setguardarfoto(false);
   }
   const guardar = async() =>{
     if (guardarboton === true) {
@@ -142,6 +156,41 @@ Swal.fire({
     }  
     setguardar(!guardarboton)
   }
+  const guardarfotosync = async() =>{
+    if (guardarboton === true) {
+      try{
+      const url = (urlmas.secure_url !== "https://res.cloudinary.com/dmgfep69f/image/upload/v1640536316/orgeial7kefv2dzsdqqt.webp")? await UploadPhoto(urlmas):urlmas;
+      socket.emit('fotouser',{
+        url,
+        uid: miusuario.uid
+        })
+    setUrl(
+      {
+      secure_url:"https://res.cloudinary.com/dmgfep69f/image/upload/v1640536316/orgeial7kefv2dzsdqqt.webp",
+      public_id: 0
+  }
+  );
+  Swal.fire({
+    position: 'top-end',
+    icon: 'success',
+    title: 'Foto Actualizada',
+    showConfirmButton: false,
+    timer: 1500
+  })
+  }catch(err){
+    console.log(err)
+    Swal.fire({
+      position: 'top-end',
+      icon: 'error',
+      title: 'Sucedio un Error',
+      showConfirmButton: false,
+      timer: 1500
+    })
+  }
+      setguardarfoto(!guardarboton)
+    }  
+    setguardarfoto(!guardarboton)
+  }
   return (
     <>
     <div className='estructuraconfig'>
@@ -157,7 +206,11 @@ Swal.fire({
        </form>
       <h1>Guillermo Peñaranda Hernandez</h1>
       <form className='containerinfousuario'>
-        <button type='button'  onClick={guardar}>{ (guardarboton === false) ? <i className='bx bxs-pencil'></i>: <i className='bx bx-save'></i>}</button>
+      <input type="file" id="filedes" aria-label="File browser example" onChange={onFilesavefoto} ></input>
+
+        <button type='button'  className='buttonr' onClick={guardar}>{ (guardarboton === false) ? <i className='bx bxs-pencil'></i>: <i className='bx bx-save'></i>}</button>
+      {(guardarboton === true || guardarfoto ===true)?<button type='button' className='buttonr buttontop' onClick={cancelar} ><i className='bx bx-x'></i></button>:null}
+        <button type='button'  className='buttonr buttonfoto' onClick={guardarfotosync}>{ (guardarfoto === false) ? <i class='bx bxs-camera' onClick={onFilefoto}></i>: <i className='bx bx-save'></i>}</button>
       <div className='detallesusuario'>
          <h2>Detalles</h2>
          <ul>
@@ -181,7 +234,7 @@ Swal.fire({
            <li><b><i className='bx bxs-user-check' ></i> Representante Legal: </b>{ (guardarboton === false) ? state.Representantelegal: <input type='text' name='Representantelegal'  placeholder={state.Representantelegal} onChange={onChangeMensaje}></input>} </li>
          </ul>
       </div>
-      <div className='fotoslocales'>
+      <div className='fotoslocales'> 
         <img src='https://i.pinimg.com/550x/94/2c/6c/942c6ce1d5875a44b851b12981f32112.jpg' alt='imagelocal'></img>
         <img src='https://propiedadescom.s3.amazonaws.com/files/292x200/boulebar-pena-flor-0-ciudad-del-sol-queretaro-queretaro-20362346-foto-01.jpg' alt='imagelocal'></img>
         <img src='https://www.oikos.com.co/constructora/images/proyectos/Comerciales-Industriales/Calera-CC/calera-cc-local.jpg' alt='imagelocal'></img>
